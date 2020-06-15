1df6bc83784ae3951f212891763b7e30cce3bf5f
DacoTaco <daco_65@hotmail.com>
2020-06-15 19:31:33 +0200

    next version is going to be 0.9 Beta 4

--- Shared/version.h
+++ Shared/version.h
@@ -23,6 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define _VERSION_H_
 
 #define VERSION		0x0000005A
-#define BETAVERSION 0x00000003
+#define BETAVERSION 0x00000004
 
 #endif
