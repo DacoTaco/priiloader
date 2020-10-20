55fa5346862fefb6b46c499995c043d4bcd62a77
DacoTaco <daco_65@hotmail.com>
2020-10-20 18:12:46 +0200

    fix typo

--- Installer/source/main.cpp
+++ Installer/source/main.cpp
@@ -183,7 +183,7 @@ void abort_pre_init(const char* msg, ...)
 	gprintf("%s -> aborting mission...\r\n", text);
 	printf("%s\nPress A to exit back to loader...\r\n",text);
 	UserYesNoStop();
-	printf("exitting...\r\n");
+	printf("exiting...\r\n");
 	printf("\x1b[%d;%dm", 37, 1);
 	VIDEO_WaitVSync();
 	exit(0);
